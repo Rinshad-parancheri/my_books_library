f53d09f49a9f37ebeb8f027375bc6b6ed7f5f4a7
rinshadpdev <rinshadp014@gmail.com>
2023-12-02 15:26:39 +0530

    change the book and library into class
    with this there is one added method which is to update the book in the dom and all dom managment is in another class

--- main.js
+++ main.js
@@ -1,138 +1,145 @@
-const addNewBookBtn = document.getElementById('newBookBtn');
-const bookContainer = document.getElementById('bookContainer');
-const overLay = document.getElementById('overlay');
-const formContaier = document.querySelector('.formContainer');
-const bookForm = document.getElementById('form');
 id = 0;
 
-const library = [];
-
-function Book(bookName, author, pages, isRead) {
-    this.name = bookName;
-    this.author = author;
-    this.pages = pages;
-    this.isRead = isRead;
-    this.id = id;
-    addBookToLibrary(this);
-}
-
-
-function addBookToLibrary(book) {
-    library[id] = book;
-    id++
-}
-
-function removeBookFromLibrary(title) {
-    const index = library.findIndex(book => book.name === title);
-    if (index !== -1) {
-        library.splice(index, 1);
+class Book {
+    constructor(bookName, author, pages, isRead) {
+        this.name = bookName;
+        this.author = author;
+        this.pages = pages;
+        this.isRead = isRead;
+        this.id = id;
+        library.addBook(this);
+        library.showBookOnDom(this);
     }
 }
 
-const book1  = new Book('rins', 'sisj', 99, true);
-const book3  = new Book('rins', 'sisj', 99, true);
-const book4  = new Book('rins', 'sisj', 99, true);
-
-function findBook(title){
-    return library.find((book) => book.name === title);
-}
-
-//form Elements
-const bookInput = document.getElementById('title')
-const authorInput = document.getElementById('author')
-const pagesInput = document.getElementById('pages')
-const readInput = document.getElementById('read');
+class Library {
+    #books = []
 
 
+    addBook(book) {
+        this.#books.push(book)
+    }
 
-// functions
-function handleFormSubmit(e) {
-    e.preventDefault();
-    console.log('hello');
-    const newBook = createBookFromFormInputs();
-    updateLibraryInDom(newBook);
-    hidTheOverlayAndForm();
-    resetForm();
-}
+    removeBook(title) {
+        const index = this.#books.findIndex(book => book.name === title);
+        if (index !== -1) {
+            this.#books.splice(index, 1);
+        } else {
+            console.log("no bok found")
+        }
+    }
 
-function createBookFromFormInputs() {
-    const bookName = bookInput.value;
-    const author = authorInput.value;
-    const pages = parseInt(pagesInput.value);
-    const isRead = readInput.checked;
-    return new Book(bookName, author, pages, isRead);
+    findBook(title) {
+        return this.#books.find((book) => book.name === title);
+    }
+
+    toggleReadStatus(book) {
+        (book.isRead) ? false : true;
+    }
+
+
+    showBookOnDom(book) {
+        domManagment.appendBook(book);
+    }
 
 }
 
-function updateLibraryInDom(book) {
-    const newBook = createBookElement(book);
-    bookContainer.appendChild(newBook);
-    toggleReadStatus(newBook,book);
-    removeBookElement(newBook);
+
+class SetUpEventListener {
+    SetUpFormAndOverLay() {
+        const bookForm = document.getElementById('form');
+        const addNewBookBtn = document.getElementById('newBookBtn');
+        document.addEventListener('DOMContentLoaded', () => {
+            bookForm.addEventListener('submit', domManagment.handleFormSubmit);
+            addNewBookBtn.addEventListener('click', domManagment.toggleFormAndOverlay);
+        });
+    }
 
 }
 
-function createBookElement(book) {
-    const newBook = document.createElement('div');
-    newBook.classList.add('book');
-    newBook.innerHTML = `
+
+class DomManagment {
+    // addNewBookBtn = document.getElementById('newBookBtn');
+    handleFormSubmit(e) {
+       e.preventDefault();
+        const bookName = document.getElementById('title').value;
+        const author = document.getElementById('author').value;
+        const pages = parseInt(document.getElementById('pages').value);
+        const isRead = document.getElementById('read').checked;
+        const newBook = new Book(bookName, author, pages, isRead);
+    }
+
+    appendBook(book) {
+        const bookContainer = document.getElementById('bookContainer');
+        const newBookDiv = this.createBookElement(book);
+        bookContainer.appendChild(newBookDiv);
+        this.toggleFormAndOverlay()
+        this.resetForm();
+    }
+
+    createBookElement(book) {
+        const newBook = document.createElement('div');
+        newBook.classList.add('book');
+        newBook.innerHTML = `
         <div class="title">${book.name}</div>
         <div class="author">${book.author}</div>
         <div class="pages">${book.pages}</div>
         <button class="isRead ${(book.isRead) ? 'read' : 'notRead'}">${book.isRead ? 'Completed' : 'Reading'}</button>
         <button class="remove">remove</button>
-    `;
-    return newBook;
-}
+        `;
+        return newBook;
+    }
 
-function toggleReadStatus(newBook) {
-    newBook.addEventListener('click', function (event) {
-        const targetButton = event.target;
-        toggleReadStatusInLibrary(targetButton);
 
-        if (targetButton.classList.contains('isRead')) {
-            targetButton.classList.toggle('read');
-            targetButton.classList.toggle('notRead');
-            targetButton.innerHTML = targetButton.classList.contains('read') ? 'Completed' : 'Reading';
-        }
-    });
-}
 
-function toggleReadStatusInLibrary(btn){
-       const title = btn.closest('.book').querySelector('.title').innerText;
-           const book = findBook(title);
-              (book.isRead)?book.isRead = false:book.isRead = true;
-
-              console.log(book)
-}
-function removeBookElement(newBook) {
-    const removeButtons = newBook.querySelectorAll('.remove');
-    removeButtons.forEach(button => {
-        button.addEventListener('click', function () {
-            button.closest('.book').remove();
-            const title = this.parentElement.querySelector('.title').innerText;
-            removeBookFromLibrary(title);
-            
+    toggleReadStatus(newBook) {
+        newBook.addEventListener('click', function (event) {
+            const targetButton = event.target;
+            const title = btn.closest('.book').querySelector('.title').innerText;
+            library.toggleReadStatus(title);
+            if (targetButton.classList.contains('isRead')) {
+                targetButton.classList.toggle('read');
+                targetButton.classList.toggle('notRead');
+                targetButton.innerHTML = targetButton.classList.contains('read') ? 'Completed' : 'Reading';
+            }
         });
-    });
+    }
+
+    resetForm() {
+        const bookForm = document.getElementById('form');
+        bookForm.reset();
+    }
+
+    toggleReadStatusInLibrary(btn) {
+
+        const book = findBook(title);
+        (book.isRead) ? book.isRead = false : book.isRead = true;
+
+
+    }
+    removeBookElement(newBook) {
+        const removeButtons = newBook.querySelectorAll('.remove');
+        removeButtons.forEach(button => {
+            button.addEventListener('click', function () {
+                button.closest('.book').remove();
+                const title = this.parentElement.querySelector('.title').innerText;
+                removeBookFromLibrary(title);
+
+            });
+        });
+    }
+
+    toggleFormAndOverlay(display) {
+        const formContainer = document.querySelector('.formContainer');
+        const overlay = document.getElementById('overlay');
+    
+        overlay.style.display = display ? 'block' : 'none';
+        formContainer.classList.toggle('active', display);
+    }
 }
 
-function resetForm() {
-    bookForm.reset();
-}
+const domManagment = new DomManagment();
+const library = new Library();
+const setUpEventListener = new SetUpEventListener();
 
-function displayFormContianerAndOverlay() {
-    overLay.style.display = 'block';
-    formContaier.classList.add('active');
-}
-
-function hidTheOverlayAndForm() {
-    overLay.style.display = 'none';
-    formContaier.classList.remove('active');
-}
-
-
-document.addEventListener('DOMContentLoaded', () => {
-    bookForm.addEventListener('submit', handleFormSubmit);
-    addNewBookBtn.addEventListener('click', displayFormContianerAndOverlay);
-});
+setUpEventListener.SetUpFormAndOverLay()
